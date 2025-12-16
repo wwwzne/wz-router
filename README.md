52b43a64f801bd378f69790428d6f1aae5a2921f
wwwzne <3466389208@qq.com>
2025-12-16 14:55:26 +0800

    'update'

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # wz-router
 
 简易前端路由控制库
+
+## 引入
+
+```javascript
+import {wzRouter} from ""
+
+```
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@
  * wz-router库
  * 前端路由控制库,适用于简单单页面网页(SPA)
  */
-type router = {
-    addRoute: (i: string, j: () => void) => void,
-    setNotFound: (i: () => void) => void
-}
-export const wzRouter: router = new class {
+export type Router = {
+    addRoute: (path: string, callback: () => void) => void;
+    setNotFound: (callback: () => void) => void;
+    navigate: (path: string) => void;
+    init: () => void;
+};
+export const wzRouter: Router = new class {
     //! 路由表
     private routes: { [key: string]: () => void } = {};
     //! 404页面
